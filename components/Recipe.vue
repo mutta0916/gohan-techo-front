57fef4cd63a9b04d7f7faeda5c86adf9da2c6ac1
mutta0916 <mutta0916@gmail.com>
2021-12-09 21:43:22 +0900

    料理一覧画面作成

--- components/Recipe.vue
+++ components/Recipe.vue
@@ -4,18 +4,17 @@
       <img :src="photo" alt="料理画像">
     </figure>
     <div class="main">
-      <div class="title-wrapper">
-        <h3>{{ recipe.name }}</h3>
-        <input type="submit" value="×" class="button_delete" @click="remove(recipe)">
-      </div>
+      <h3>{{ recipe.name }}</h3>
       <p class="memo">
         {{ recipe.memo }}
       </p>
+      <span class="genre_label">{{ recipe.genre }}</span>
+      <span v-for="(type, index) in recipe.type" :key="index" class="type_label">{{ type }}</span>
     </div>
   </NuxtLink>
 </template>
 
-<script>
+<script scoped>
 export default {
   props: {
     recipe: {
@@ -29,30 +28,32 @@ export default {
     return {
       photo: require('../assets/upload.svg')
     }
-  },
-  methods: {
-    remove (recipe) {
-      this.$store.commit('remove', recipe)
-    }
   }
 }
 </script>
 
 <style scoped>
 h3 {
+  font-size: 17px;
   margin: 0;
 }
 
 .item {
   display: flex;
-  align-items: flex-start;
+  flex-flow: column;
   justify-content: space-between;
+  text-decoration: none;
+  color: currentColor;
+}
+
+.item:hover {
+  opacity: 0.7;
 }
 
 .img_wrapper {
   flex: 0 1 47%;
   margin: 0;
-  margin-right: 3%;
+  margin-right: 10px;
 }
 
 img {
@@ -60,20 +61,34 @@ img {
 }
 
 .main {
-  flex: 1;
+  flex:0 1 50%;
 }
 
-.title-wrapper {
-  display: flex;
-  justify-content: space-between;
+span {
+  font-size: 14px;
+  margin-right: 5px;
+  text-align: center;
+  border-radius: 20%;
+  padding: 5px;
+  color: #fff;
+}
+.genre_label {
+  background-color: #fc90b9;
 }
 
-.button_delete {
-  font-size: 10px;
-  width: 16%;
-  background-color: rgb(255, 155, 155);
-  border:solid 1px #fff;
-  border-radius: 50%;
+.type_label {
+  background-color: #8bd3dd;
+}
+
+@media only screen and (min-width: 768px) {
+  .item {
+    flex-flow: row;
+    align-items: flex-start;
+  }
+
+  h3 {
+    font-size: 19px;
+  }
 }
 </style>
 }
--- pages/recipeListApp.vue
+++ pages/recipeListApp.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <div class="content_wrapper">
+      <input v-model="keyword" type="text" placeholder="ここで検索ができます。" class="search">
+      <h1>Menu</h1>
       <div class="list">
-        <recipe v-for="(recipe, index) in recipes" :key="index" :recipe="recipe" class="each_recipe" />
+        <recipe v-for="(recipe, index) in filteredRecipes" :key="index" :recipe="recipe" class="each_recipe" />
       </div>
     </div>
   </div>
@@ -15,29 +17,56 @@ export default {
   components: {
     recipe
   },
+  data () {
+    return {
+      keyword: ''
+    }
+  },
   computed: {
     recipes () {
       return this.$store.state.recipes
+    },
+    filteredRecipes () {
+      const searchRecipes = []
+      for (const i in this.recipes) {
+        const recipe = this.recipes[i]
+        if (recipe.name.includes(this.keyword) ||
+              recipe.genre.includes(this.keyword) ||
+                recipe.type.includes(this.keyword)) {
+          searchRecipes.push(recipe)
+        }
+      }
+      return searchRecipes
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+.search {
+  margin: 5px;
+}
+
+h1 {
+  text-align: center;
+  margin: 0 0 30px 0;
+  letter-spacing: 10px;
+}
+
 .content_wrapper {
   max-width: 1200px;
   margin: 15px auto;
-  padding: 50px;
+  padding: 0 30 0 30px;
 }
 
 .list {
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+  justify-content: center;
 }
 
 .each_recipe {
-  flex: 0 1 30%;
-  margin-bottom: 20px;
+  flex: 0 1 40%;
+  margin: 20px;
 }
 </style>
