4980564df021d71cb47d0c5199ec196d4c384e6c
mutta0916 <mutta0916@gmail.com>
2021-12-28 11:09:12 +0900

    テストコード修正

--- test/Sample.spec.js
+++ test/Sample.spec.js
@@ -1,8 +1,6 @@
-import { mount } from '@vue/test-utils'
-
 describe('SampleTest', () => {
   test('test', () => {
-    const result = 1 + 3;
-    expect(result).toBe(4);
+    const result = 1 + 3
+    expect(result).toBe(4)
   })
 })
