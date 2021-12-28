9917384ae3207bcd560903efe0f19e9d41978fd8
mutta0916 <mutta0916@gmail.com>
2021-12-28 10:55:02 +0900

    料理作成画面にジャンルと種類を追加

--- store/index.js
+++ store/index.js
@@ -4,16 +4,16 @@ const createStore = () => {
   return new Vuex.Store({
     state: () => ({
       recipes: [
-        { name: '豚キムチ', genre: '和食', type: ['主菜'], servings: '2', memo: '＠＠＠＠＠＠＠＠＠１＠＠＠＠＠＠＠＠＠２＠＠＠＠＠＠＠＠＠３＠＠＠＠＠＠＠＠＠４＠＠＠＠＠＠＠＠＠５' },
-        { name: 'カレーライス', genre: '和食', type: ['主食', '主菜'], servings: '3', memo: 'すごくおいしかった！' },
-        { name: '肉うどん', genre: '和食', type: ['主食', '主菜'], servings: '1', memo: 'お肉大盛！' },
-        { name: '焼肉', genre: '和食', type: ['主菜'], servings: '2', memo: 'カルビ多め' },
-        { name: 'チキンのトマト煮', genre: '洋食', type: ['主菜'], servings: '3', memo: '鶏むね肉固くなりがち' },
-        { name: '常夜鍋', genre: '和食', type: ['主菜'], servings: '2', memo: 'ほうれん草たっぷり' },
-        { name: 'サラダチキン', genre: '洋食', type: ['主菜'], servings: '4', memo: 'ダイエットにはこれ' },
-        { name: '豚肉と白菜のくたくた煮', genre: '和食', type: ['主菜'], servings: '2', memo: 'くったり白菜はうまい' },
-        { name: 'キムチ鍋', genre: '和食', type: ['主菜'], servings: '4', memo: 'がっつり食べたいときに' },
-        { name: 'カルボナーラ', genre: '洋食', type: ['主菜'], servings: '4', memo: 'これぞデブ飯！' }
+        { name: '豚キムチ', genre: '和食', type: '主菜', servings: '2', memo: '＠＠＠＠＠＠＠＠＠１＠＠＠＠＠＠＠＠＠２＠＠＠＠＠＠＠＠＠３＠＠＠＠＠＠＠＠＠４＠＠＠＠＠＠＠＠＠５' },
+        { name: 'カレーライス', genre: '和食', type: '丼', servings: '3', memo: 'すごくおいしかった！' },
+        { name: '肉うどん', genre: '和食', type: '麺', servings: '1', memo: 'お肉大盛！' },
+        { name: '焼肉', genre: '和食', type: '主菜', servings: '2', memo: 'カルビ多め' },
+        { name: 'チキンのトマト煮', genre: '洋食', type: '主菜', servings: '3', memo: '鶏むね肉固くなりがち' },
+        { name: '常夜鍋', genre: '和食', type: '主菜', servings: '2', memo: 'ほうれん草たっぷり' },
+        { name: 'サラダチキン', genre: '洋食', type: '主菜', servings: '4', memo: 'ダイエットにはこれ' },
+        { name: '豚肉と白菜のくたくた煮', genre: '和食', type: '主菜', servings: '2', memo: 'くったり白菜はうまい' },
+        { name: 'キムチ鍋', genre: '和食', type: '主菜', servings: '4', memo: 'がっつり食べたいときに' },
+        { name: 'カルボナーラ', genre: '洋食', type: '主菜', servings: '4', memo: 'これぞデブ飯！' }
       ]
     }),
     mutations: {
